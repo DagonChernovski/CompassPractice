6119c7db4adbc48722bace8c2740edd6d1b78682
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2024-07-13 18:14:47 +0500

    Delete models/room.js

--- models/room.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const Room = sequelize.define('Room', {
-    // Define your room attributes
-    name: { type: DataTypes.STRING, allowNull: false },
- });
-
- module.exports = Room;
